5487537787155ec8aaffd4d84598447d1a67468e
NAS <nowikow96@mail.ru>
2025-03-30 14:56:22 +0300

    first

---
+++ main.py
@@ -0,0 +1,30 @@
+from aiogram import Dispatcher, Bot
+from aiogram.types import Message
+from aiogram.filters import Command
+
+from config_bot import My_Bot
+
+my_bot = My_Bot()
+
+dp = Dispatcher()
+bot = Bot(token=my_bot.token)
+
+
+@dp.message(Command(commands=["start"]))
+async def start(message=Message):
+    await message.answer(text=my_bot.start)
+
+
+@dp.message(Command(commands=["help"]))
+async def help(message=Message):
+    await message.answer(text=my_bot.help)
+
+
+@dp.message()
+async def text_mess(message=Message):
+    await message.answer(text=message.text)
+
+
+if __name__ == "__main__":
+    dp.run_polling(bot)
+    print("Бот запущен")
